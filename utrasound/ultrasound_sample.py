a0fef2f71dc0fb095cfec8df8a813fc19e28e813
orange2000121 <danny1.2104@gmail.com>
2023-08-30 19:05:14 +0800

    add ros

--- utrasound/ultrasound_sample.py
+++ utrasound/ultrasound_sample.py
@@ -16,9 +16,10 @@ def readus(port1):
     command = bytearray(command)
     # while 1 :
     port1.write(command)
-    time.sleep(0.25)
+    time.sleep(0.5)
     rcv1 = port1.read(1)
-    #print(rcv)
+    time.sleep(0.5)
+    print(rcv1)
     head = ord(rcv1)
     #print(head)
     if(head == 85):
@@ -46,7 +47,8 @@ def readus(port1):
 
 
 if __name__ == '__main__':
-    port1 = serial.Serial("COM7", baudrate = 9600, timeout = 2)
+    print('hello')
+    port1 = serial.Serial("/dev/tty.usbserial-W2100850", baudrate = 9600, timeout = 2)
     plt.ion() #开启interactive mode 成功的关键函数
     plt.figure(1)
     t = []
@@ -73,13 +75,13 @@ if __name__ == '__main__':
             plt.xlabel('                time')
             plt.plot(t,ds[i-1],'-r')
         #plt.figure(2)
-        for i in range(5,9):
-            plt.subplot(2, 4, i)
-            plt.title("bs"+str(i-4))
-            plt.ylim(250,4500)
-            plt.ylabel('(mm)')
-            plt.xlabel('time')
-            plt.plot(t,ds[i-1],'-b')
+        # for i in range(5,9):
+        #     plt.subplot(2, 4, i)
+        #     plt.title("bs"+str(i-4))
+        #     plt.ylim(250,4500)
+        #     plt.ylabel('(mm)')
+        #     plt.xlabel('time')
+        #     plt.plot(t,ds[i-1],'-b')
         plt.pause(0.01)
 
     plt.savefig('senor1.png')#儲存圖片
