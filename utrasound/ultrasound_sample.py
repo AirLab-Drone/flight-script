1ced3f149ee60196d931fe1389e0850892ba442c
“orange2000121” <danny1.2104@gmail.com>
2023-11-07 16:55:36 +0800

    Removed unused file and added range finder functionality

--- utrasound/ultrasound_sample.py
+++ utrasound/ultrasound_sample.py
@@ -1,7 +1,10 @@
 import serial
 import time
-# import matplotlib.pyplot as plt
+from dronekit import connect, VehicleMode
+from pymavlink import mavutil
+import threading
 
+global dis 
 def sensor(H,L):
     Dis = int(H)*256 + int(L)
     return Dis
@@ -15,17 +18,18 @@ def readus(port1):
     command = [0x55,0xAA,0x01,0x01,0x01]
     command = bytearray(command)
     # while 1 :
+    # port1.flushInput()
     port1.write(command)
-    time.sleep(0.5)
+    time.sleep(0.3)
     rcv1 = port1.read(1)
-    time.sleep(0.5)
     print(rcv1)
     head = ord(rcv1)
-    #print(head)
+    print(head)
     if(head == 85):
-        for i in range(len(in_byte)):
+        for i in range(len(in_byte)-2):
             rcv1 = port1.read(1)
             in_byte[i] = ord(rcv1)
+            # print(in_byte[i])
         #print("received = ", in_byte2)
         checksum = 0x55
         for i in range(0,11):
@@ -43,50 +47,83 @@ def readus(port1):
         print("S = ", fs1, ",", fs2 ,",", fs3 ,",", fs4)
         # period = 250 ms
         # time.sleep(0.25)
-    return fs1,fs2,fs3,fs4
+        print(in_byte)
+        return fs1,fs2,fs3,fs4
+    return 0,0,0,0
+#單個讀取
+def readusOne(port1,read=1):#read=1~4
+    in_byte1 = [0,0,0,0,0,0]
+    #0X55 0XAA 0X01 0X01 checksum
+    a = 0x10 + read-1
+    checksum = sum([0x55 , 0xAA , 0x01 , a]) & 0x00FF
+    command = [0x55,0xAA,0x01,a,checksum]
+    command = bytearray(command)
+    port1.write(command)
+    # period = 90 ms
+    time.sleep(0.1)#單個讀取的響應時間比較快
+    rcv1 = port1.read(1)
+    head = ord(rcv1)
+    if(head == 85):
+        checksum = 0x55
+        for i in range(len(in_byte1)):
+            rcv1 = port1.read(1)
+            in_byte1[i] = ord(rcv1)
+            if i < 5:
+                checksum += in_byte1[i]
+        checksum = checksum & 0x00FF
+        print('check sum: ',checksum)
+        if checksum == in_byte1[5]:#package not loss
+            s = sensor(in_byte1[3], in_byte1[4])
+            print(f'raw s: {s}')
+            if s == 0:s=4500 #超出量程
 
-
+        else:#package loss
+            s = 9999
+    else:
+        s = 9999
+    return s
+def sendRangeFinder(distance):
+    # 创建一个测距仪消息
+    msg = vehicle.message_factory.distance_sensor_encode(
+        0,  # 时间微秒（0表示当前时间）
+        25,                         # 最小距离
+        450,                       # 最大距离
+        distance,                  # 当前距离
+        1,                         # 类型（0表示超声波）
+        0,                         # ID
+        mavutil.mavlink.MAV_SENSOR_ROTATION_PITCH_270, # 方向（0表示向下）
+        0                          # 信号强度
+    )
+    # 发送测距仪消息
+    # print("sendRangeFinder: ",distance,"cm")
+    vehicle.send_mavlink(msg)
+def send_distance():
+    global dis
+    while True:
+        try:
+            if(dis == 9999):
+                continue
+            sendRangeFinder(int(dis/10))
+            time.sleep(0.05)
+        except:
+            continue
+def read_distance():
+    global dis
+    while True:
+        try:
+            dis = readusOne(port1,1)
+            print("dis=",dis)
+        except Exception as e: 
+            print(e)
+            continue
 if __name__ == '__main__':
-    print('hello')
-    port1 = serial.Serial("/dev/tty.usbserial-W2100850", baudrate = 9600, timeout = 2)
-    plt.ion() #开启interactive mode 成功的关键函数
-    plt.figure(1)
-    t = []
-    t_now = 0
-    ds=[ds1,ds2,ds3,ds4] = [[],[],[],[]]
-    i=0
-    # plt.clf()#清空画布上的所有内容
-    for i in range(0,40):
-        #print(readus(port1))
-        s1,s2,s3,s4 = readus(port1)
-        print("num=",i,",s1=",s1,",s2=",s2,",s3=",s3,",s4=",s4)
-        t_now = i*0.5
-        t.append(t_now)#模拟数据增量流入，保存历史数据
-        ds1.append(s1)
-        ds2.append(s2)
-        ds3.append(s3)
-        ds4.append(s4)
-        # plt.figure(1)
-        # for i in range(1,5):
-        #     plt.subplot(2, 4, i)
-        #     plt.title("fs"+str(i))
-        #     plt.ylim(250,4500)
-        #     plt.ylabel('(mm)')
-        #     plt.xlabel('                time')
-        #     plt.plot(t,ds[i-1],'-r')
-        #plt.figure(2)
-        # for i in range(5,9):
-        #     plt.subplot(2, 4, i)
-        #     plt.title("bs"+str(i-4))
-        #     plt.ylim(250,4500)
-        #     plt.ylabel('(mm)')
-        #     plt.xlabel('time')
-        #     plt.plot(t,ds[i-1],'-b')
-        plt.pause(0.01)
-
-    # plt.savefig('senor1.png')#儲存圖片
-    # plt.show()
-
-
-
-
+    global dis
+    port1 = serial.Serial("/dev/serial0", baudrate = 9600, timeout = 2)
+    vehicle = connect('/dev/ttyACM0',baud=9600)
+    dis = 0
+    thread1 = threading.Thread(target=read_distance)
+    thread2 = threading.Thread(target=send_distance)
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
